a52dfac200d8f31d84d507bc105c32da78368fe5
Sam Foster <slfotg@gmail.com>
2018-12-04 21:49:07 -0600

    Updated install script

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# Check for existing file and remove if they exist
+if [ -f ~/.vimrc ]; then
+    rm -rf ~/.vimrc
+fi
+
+if [ -f ~/.vim ]; then
+    rm ~/.vim
+fi
+
+# Create symlinks for vim files
 ln -sf `pwd`/.vimrc ~/.vimrc
 ln -sf `pwd`/.vim ~/.vim
